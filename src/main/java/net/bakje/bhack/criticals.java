5ba1b17f8038883350e4493b31a2a0d072fdfd8c
bakje <fambubberman@gmail.com>
2022-01-15 19:36:56 +0100

    the criticals makes block lag and ghost blocks on ecme lemme try this bleachhack skid and see if it does the same

--- src/main/java/net/bakje/bhack/criticals.java
+++ src/main/java/net/bakje/bhack/criticals.java
@@ -35,8 +35,16 @@ public class criticals implements ClientModInitializer {
                         double z = mc.player.getZ();
                         // basically this makes you go up and down a little the entire time
                         // this sometimes needs you to sprint, use RageSprint
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.0633, z, false));
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y, z, false));
+
+                        //mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.0633, z, false));
+                        //mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y, z, false));
+
+                        //bleachhack skid below
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.42, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.65, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.72, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.53, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.32, z, false));
                     }
                 }
             }
