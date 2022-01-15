562ea94a97fce23191f4b60afdd893d4149452eb
bakje <fambubberman@gmail.com>
2022-01-15 16:06:56 +0100

    idk bruh speed funny onyl works on servers without anticheat / singleplayer

--- src/main/java/net/bakje/bhack/badspeed.java
+++ src/main/java/net/bakje/bhack/badspeed.java
@@ -1,6 +1,5 @@
 package net.bakje.bhack;
 
-import io.netty.util.internal.MathUtil;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -8,30 +7,29 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 
-public class badfly implements ClientModInitializer {
+public class badspeed implements ClientModInitializer {
 
-    private boolean bruhfly;
+    private boolean bruhspeed;
 
     @Override
     public void onInitializeClient() {
 
-        KeyBinding binding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("bruhfly", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "bhack"));
-        bruhfly=!bruhfly;
+        KeyBinding binding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("bruhspeed", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "bhack"));
+        bruhspeed =!bruhspeed;
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             MinecraftClient mc = MinecraftClient.getInstance();
 
             while (binding1.wasPressed()) {
-                client.player.sendMessage(new LiteralText("[bhack] Set bruhfly to " + bruhfly), false);
-                bruhfly=!bruhfly;
+                client.player.sendMessage(new LiteralText("[bhack] Set bruhspeed to " + bruhspeed), false);
+                bruhspeed =!bruhspeed;
             }
 
-            if(!bruhfly) {
-                if (mc.player != null)
-                    return;
-                mc.player.setVelocity(mc.player.getVelocity().x*0.1,0.1,mc.player.getVelocity().z*0.1);
+            if(!bruhspeed) {
+                if (mc.player != null) {
+                    mc.player.setVelocity(mc.player.getVelocity().x*1.1, mc.player.getVelocity().y, mc.player.getVelocity().z*1.1);
+                }
             }
         });
     }
