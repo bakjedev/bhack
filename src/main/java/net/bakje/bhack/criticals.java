fc4cc4736a96f6a9d6417970eddfed9ee6a5fc9a
bakje <fambubberman@gmail.com>
2022-01-15 19:02:17 +0100

    added a somehow working criticals like how the fuck am i this good at the game

---
+++ src/main/java/net/bakje/bhack/criticals.java
@@ -0,0 +1,42 @@
+package net.bakje.bhack;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
+
+public class criticals implements ClientModInitializer {
+
+    private boolean crits;
+
+    @Override
+    public void onInitializeClient() {
+        KeyBinding binding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("Criticals", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_P, "bhack"));
+        crits =!crits;
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            MinecraftClient mc = MinecraftClient.getInstance();
+
+            while (binding1.wasPressed()) {
+                client.player.sendMessage(new LiteralText("[bhack] Set Criticals to " + crits), false);
+                crits =!crits;
+            }
+
+            if (!crits){
+                if (mc.player != null) {
+                    if (mc.player.isOnGround()) {
+                        double x = mc.player.getX();
+                        double y = mc.player.getY();
+                        double z = mc.player.getZ();
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.06, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y, z, false));
+                    }
+                }
+            }
+        });
+    }
+}
