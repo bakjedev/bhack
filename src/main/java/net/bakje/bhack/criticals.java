c36c535064f9febd2a4b4d1c48d558d00b8a6d66
bakje <fambubberman@gmail.com>
2022-01-15 19:47:26 +0100

    reverted criticals bleach skid to old bad criticals

--- src/main/java/net/bakje/bhack/criticals.java
+++ src/main/java/net/bakje/bhack/criticals.java
@@ -29,23 +29,17 @@ public class criticals implements ClientModInitializer {
             if (!crits){
                 if (mc.player != null) {
                     if (mc.player.isOnGround()) {
-                        // gets players current pos
+                        //gets players current pos
                         double x = mc.player.getX();
                         double y = mc.player.getY();
                         double z = mc.player.getZ();
-                        // basically this makes you go up and down a little the entire time
-                        // this sometimes needs you to sprint, use RageSprint
+                        //basically this makes you go up and down a little the entire time
+                        //this sometimes needs you to sprint, use RageSprint
+                        //ALSO DONT USE THIS on ecme when you mine blocks you get block lag and ghost blocks, idk how fix
 
-                        //mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.0633, z, false));
-                        //mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y, z, false));
-
-                        //bleachhack skid below
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.42, z, false));
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.65, z, false));
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.72, z, false));
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.53, z, false));
-                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.32, z, false));
-                    }
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.0633, z, false));
+                        mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y, z, false));
+                  }
                 }
             }
         });
