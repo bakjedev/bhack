1edcaf32be7a982273b84f0c95f90816eb633e99
Eskerepvp <75771072+Eskerepvp@users.noreply.github.com>
2022-01-18 03:07:36 -0800

    bajke more like blackj

---
+++ src/main/java/net/bakje/bhack/util/TimerUtil.java
@@ -0,0 +1,35 @@
+package net.bakje.bhack.util;
+
+public class TimerUtil {
+    private long time;
+
+    public TimerUtil()
+    {
+        time = -1;
+    }
+
+    public boolean passed(double ms)
+    {
+        return System.currentTimeMillis() - this.time >= ms;
+    }
+
+    public void reset()
+    {
+        this.time = System.currentTimeMillis();
+    }
+
+    public void resetTimeSkipTo(long p_MS)
+    {
+        this.time = System.currentTimeMillis() + p_MS;
+    }
+
+    public long getTime()
+    {
+        return time;
+    }
+
+    public void setTime(long time)
+    {
+        this.time = time;
+    }
+}
